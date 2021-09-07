0b083ad8240014e0dd9b08ef3dcf75db6d489d2f
lhernandez19 <diegotapia1987@gmail.com>
2021-09-06 23:24:26 -0500

    first commit

---
+++ assets/js/lab1app.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+	$("#giYes").click(function () {
+		$("#hideDetailsGI").show();
+	});
+	$("#giNo").click(function(){
+		$("#hideDetailsGI").hide();
+	});
+
+	$("#giYes2").click(function () {
+		$("#hideDetailsGI2").show();
+	});
+	$("#giNo2").click(function(){
+		$("#hideDetailsGI2").hide();
+	});
+
+});
